ce23e343383f901d959d6b5372c5f38dfa9e6d9a
juliocadenas <juliocadenas@gmail.com>
2025-05-30 10:48:03 -0400

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -178,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8aa60b22f15a9c50")
+/******/ 		__webpack_require__.h = () => ("b00e6647e5b6f42c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
